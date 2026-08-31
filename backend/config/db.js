462eae339e7655e2c597a38e0d46c9e8a182e484
redeaprendiz904 <redeaprendiz904@gmail.com>
2026-08-31 15:51:10 -0300

    atualizações no index.html

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,7 +4,7 @@ const mysql = require("mysql2");
 const db = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "Admin",
+    password: "admin",
     database: "rede_aprendiz"
 });
 
